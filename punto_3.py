78341d86f3119e3b5ce7c7253694d467e144a51e
Fabian0611 <164115415+Fabian0611@users.noreply.github.com>
2024-11-25 00:19:47 -0500

    Add files via upload

---
+++ punto_3.py
@@ -0,0 +1,19 @@
+def primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def filtrar_primos(lista):
+    return [num for num in lista if primo(num)]
+
+# Solicita la lista de números al usuario
+
+numeros = input("Introduce una lista de números separados por espacios: ")
+lista_numeros = [int(x) for x in numeros.split(" ")]
+
+# Llama a la función y muestra los números primos
+
+print("Números primos:", filtrar_primos(lista_numeros))
